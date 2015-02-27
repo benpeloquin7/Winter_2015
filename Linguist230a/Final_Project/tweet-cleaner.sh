83e43ee83289ef47c7de4a74f40c954de5564f08
Benjamin Nathan Peloquin <bpeloqui@corn27.stanford.edu>
2015-02-26 18:21:08 -0800

    improved cleaner script - newlines, http links, backslash, RT,

--- Linguist230a/Final_Project/tweet-cleaner.sh
+++ Linguist230a/Final_Project/tweet-cleaner.sh
@@ -1,5 +1,14 @@
+###:---------tweet-cleaner.sh-----------:###
+#Removes http links, twitter handles, new lines and back slashes
+
 path="data-hold/account-store/"
 tweets=$1
 
+if [[ -e "cleantest.txt" ]]; then
+rm cleantest.txt
+fi
+
 csvfix order -f 4 $path$tweets | grep -v "RT" |\
-tr '[A-Z]' '[a-z]' | tr -d "\"" > cleantest.txt
+sed -E 's/http.+\b//g' | sed -E 's/@.+\b//g' |\
+tr -d '\"' | tr -d '\n' | tr [A-Z] [a-z] > cleantest.txt
+
